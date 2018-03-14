c35c15e96382aaf7203fcf665d24061bcf8dbd87
Ozone-Layer <37383967+Ozone-Layer@users.noreply.github.com>
2018-03-14 16:40:17 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -5,11 +5,6 @@ client.on('ready', () => {
     console.log('I am ready!');
 });
 
-client.on('message', message => {
-    if (message.content == 'ping') {
-    	message.reply('pong');
-  	}
-});
 
 // THIS  MUST  BE  THIS  WAY
 client.login(process.env.BOT_TOKEN);
